0e25e4551622eb6b9d74151e33a56665f5ac525b
Erisa A <seriel@erisa.moe>
2021-06-27 10:25:56 +0100

    Change/remote references to Erisa/Gold

--- script.js
+++ script.js
@@ -3,6 +3,3 @@ document.write('You\'re always free to reach out to me at my email address: ' + 
 '#' + '3' + '7' + ';' + '7' + '2' + 'i' + 's' + 'a' + '&' + '#' + '4' + '6' + ';' + 'm' + 'o' + '%' + '&' + '#' + '5' + '4' + ';' + '5' + "'" + '>' +
 '&' + '#' + '1' + '1' + '5' + ';' + 'e' + 'r' + 'i' + 'e' + 'l' + '&' + '#' + '6' + '4' + ';' + 'e' + 'r' + 'i' + 's' + 'a' + '&' + '#' + '4' + '6' +
 ';' + '&' + '#' + '1' + '0' + '9' + ';' + 'o' + 'e' + '<' + '/' + 'a' + '>');
-
-// I'm inside a script anyway, might as well make the most of it.
-console.log("Hey there! You can view and contribute to the source of this website here: https://github.com/Erisa/erisa.moe\nWeb development is not something I usually do, so I appreciate any help.");
